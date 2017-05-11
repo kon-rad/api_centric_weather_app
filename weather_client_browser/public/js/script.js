58723ce297c1508a5d6dfb191e3a36edf67fe0f0
konradgnat <konradmgnat@gmail.com>
2017-05-11 14:59:03 -0500

    Add version with ajax connection between client and api

---
+++ weather_client_browser/public/js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+
+var weather_api = "http://localhost:8000?callback=?";
+
+function httpGetAsync(theUrl, callback) {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() {
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+			callback(xmlHttp.responseText);
+	}
+	xmlHttp.open("GET", theUrl, true); // true is for async
+	xmlHttp.send(null);
+}
+
+function displayResult(data) {
+	document.getElementById('display').innerHTML = json;
+}
+
+$("#weather_btn").click(function() {
+	console.log("clicked");
+	$.ajax({
+		type:"GET",
+		dataType:"jsonp",
+		url: "http://localhost:8000?callback=?",
+		success: function(data) {
+			console.log("works");
+			console.log(data.hello);
+			$('#display').text(data.hello);
+		}
+
+	});
+});
+
+	
+})
---
+++ weather_client_browser/server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var request = require('superagent');
+
+// create the express app
+var app = express();
+var router = express.Router();
+
+// set the view engine to use EJS and set default views directory
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/public/views');
+
+// tell express which directory to serve static assets like css, images, etc.
+app.use(express.static(__dirname + '/public'));
+
+// client id and secret from AuthoO Client
+// var NON_INTERACTIVE_CLIENT_ID = '';
+// var NON_INTERACTIVE_CLIENT_SECRET = '';
+
+// define object to use to exchange credentials for access token.
+// var authData = {
+// 	client_id: 	NON_INTERACTIVE_CLIENT_ID,
+// 	client_secret: NON_INTERACTIVE_CLIENT_SECRET,
+// 	grant_type: 'client_credentials',
+// 	audience: ''
+// }
+
+// render the homepage 
+app.get('/', function(req, res){
+	res.render('index');
+})
+
+// app.get('/getWeather', function(req, res)
+// 	.get('http://localhost:8000')
+// 	.end(function(err, data) {
+// 		if (data.status == 403) {
+// 			res.send(403, '403 forbidden');
+// 		} else {
+// 			var weather = data.body;
+// 			res.send(weather);
+// 		}
+// 	})
+
+// set weather website to listen on port 3000
+app.listen(3000);
+console.log("running on port 3000");
+
